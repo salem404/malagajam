1841c6657ea8018acd7f28ed3af676a9b8cfbabe
salem404 <salem4O4@proton.me>
2026-01-31 02:26:17 +0100

    feat: ✨ Añadido cambio de color y rework

--- scripts/trozo.gd
+++ scripts/trozo.gd
@@ -1,8 +1,28 @@
 class_name Trozo
-extends Node
+extends Resource
 
 @export var id: int
-var category: String
-var atributos: Dictionary
+@export var category: String
+@export var atributos: Dictionary
+@export var preset_color: Color
 
 
+
+const CATEGORIES := [
+	"cabeza",
+	"ojos",
+	"nariz",
+	"boca",
+]
+
+const ATRIBUTOS_POSIBLES := [
+	"cuqui",
+	"macabro"
+]
+
+
+func _init(p_id: int, p_category: String = "", p_atributos: Dictionary = {},p_color: Color = Color("#FFFFFF")):
+	id = p_id
+	category = p_category
+	atributos = p_atributos
+	preset_color = p_color
--- scripts/trozos_registry.gd
+++ scripts/trozos_registry.gd
@@ -1,52 +1,32 @@
 extends Node
 
-# TODO: Investigar si es mejor usar Resources
-
-const CATEGORIES := [
-	"ojos",
-	"cabeza",
-]
-
-# Atributos de relleno
-const ATRIBUTOS_POSIBLES := [
-	"fuerza",
-	"agilidad",
-	"inteligencia",
-	"carisma",
-	"suerte",
-]
-
-const TROZOS_REGISTRY := {
-	"antifaz_rosa": {
-		"id": 1,
-		"category": "ojos",
-		"atributos": {
-			"carisma": 5,
-			"suerte": 2,
-		},
-	},
-	"lagrimas": {
-		"id": 2,
-		"category": "ojos",
-		"atributos": {
-			"inteligencia": 3,
-			"carisma": -1,
-		},
-	},
-	"cuernos_demonio": {
-		"id": 3,
-		"category": "cabeza",
-		"atributos": { },
-	},
+var trozos_registry := {
+	"antifaz_rosa": Trozo.new(
+		1,
+		"ojos",
+		{"cuqui": 1},
+		Color("#FF69B4"),
+		),
+	"lagrimas": Trozo.new(
+		2,
+		"ojos",
+		{"macabro": 2},
+		Color("#0000FF"),
+		),
+	"cuernos_demonio": Trozo.new(
+		3,
+		"cabeza",
+		{"macabro": 3},
+		Color("#8B0000"),
+		)
 }
 
 
 func get_trozo(id: int) -> Trozo:
-	for trozo_data in TROZOS_REGISTRY.values():
-		if trozo_data["id"] == id:
-			var trozo = Trozo.new()
-			trozo.id = trozo_data["id"]
-			trozo.category = trozo_data["category"]
-			trozo.atributos = trozo_data["atributos"]
+	for trozo in trozos_registry.values():
+		if trozo.id == id:
 			return trozo
 	return null
+
+func get_all_trozos() -> Dictionary:
+	return trozos_registry
